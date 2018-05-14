ed849d4f1c3af4d73519cdd3c060b290fc7c2d42
Brad Pinter <brad.pinter@gmail.com>
2018-05-14 14:52:23 +0000

    tweak skaffold's install paths

--- Food/skaffold.lua
+++ Food/skaffold.lua
@@ -15,7 +15,7 @@ food = {
             sha256 = "61288c92042d65f2bd7fe31f0d7b482986baadab62dbd26c00dd7de1fdff6c0f",
             resources = {
                 {
-                    path = name,
+                    path = name .. "-darwin-amd64",
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -29,7 +29,7 @@ food = {
             sha256 = "7875a84fa3fe10088769daac0814d91a8744192622d02d8c203f7a503413d1ff",
             resources = {
                 {
-                    path = name,
+                    path = name .. "-linux-amd64",
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -38,7 +38,7 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/GoogleContainerTools/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-windows-amd64.exe"
+            url = "https://github.com/GoogleContainerTools/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "-windows-amd64.exe",
             -- shasum of the release archive
             sha256 = "53b8156e4e0e6275ac7055a509de130f2f45009c3e6886a871fb51dab8e026db",
             resources = {
